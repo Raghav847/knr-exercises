afe1200a7cfb2bd4c53054449499ca9e8d73dfd4
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-05-18 23:50:31 -0700

    ex 1.3 & 1.4

---
+++ ch01/1-4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float fahr, celsius;
+	float lower, upper, step;
+
+	lower = 0;        /* lower limit of temperature scale */
+	upper = 300;      /* upper limit */
+	step  = 20;       /* step size */
+
+	fahr = celsius = lower;
+
+	printf("Celsius\t\tFahrenheit\n");
+
+	while (celsius <= upper) {
+		fahr = celsius * 9 / 5 + 32;
+		printf("%7.0f\t%18.0f\n", celsius, fahr);
+		fahr = fahr + step;
+		celsius = celsius + step;
+	}
+	return 0;
+}
